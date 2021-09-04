e68a36ed603b33e4697a5a4db3ea05eeb73911b4
bilcadina <adina15felicia@yahoo.com>
2021-09-04 10:47:38 +0300

    gitignore

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 0
   };
+
+  handleIncrement(){
+    console.log('Increment Clicked');
+  }
 
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
-  }
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
-  }
 }
 
+getBadgeClasses() {
+  let classes = "badge m-2 badge";
+  classes += this.state.count === 0 ? "warning" : "primary";
+  return classes;
+}
 export default Counter;
