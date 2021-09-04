40cf1cc9694b01f4a42ae3c68f98305919b7535d
bilcadina <adina15felicia@yahoo.com>
2021-09-04 10:53:19 +0300

    counter jsx corrected

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
   };
 
-  handleIncrement(){
-    console.log('Increment Clicked');
+  handleIncrement() {
+    console.log("Increment Clicked");
   }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
-}
+  }
 
-getBadgeClasses() {
-  let classes = "badge m-2 badge";
-  classes += this.state.count === 0 ? "warning" : "primary";
-  return classes;
+  getBadgeClasses() {
+    let classes = "badge m-2 badge";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
 export default Counter;
